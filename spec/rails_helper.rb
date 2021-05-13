9615b8f1410b83d0da3568f7ed25a6867e388dfa
cristichirtos <79589737+cristichirtos@users.noreply.github.com>
2021-05-13 15:46:57 +0300

    Add empty lines in rspec generated file

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,12 +4,14 @@ require File.expand_path('../config/environment', __dir__)
 
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
 RSpec.configure do |config|
 
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
